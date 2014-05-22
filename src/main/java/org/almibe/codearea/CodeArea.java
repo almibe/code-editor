9bc015ade76f2f82b298b89e58fcd7899d9adce4
Alex Berry <berry3@indiana.edu>
2014-05-22 00:56:07 -0400

    First pass of interacting with CodeArea's webview (get/set value & mode) Removed default mode and theme from editor.html Added some tests to run once I figure out timing issues

--- src/main/java/org/almibe/codearea/CodeArea.java
+++ src/main/java/org/almibe/codearea/CodeArea.java
@@ -1,18 +1,78 @@
 package org.almibe.codearea;
 
-
-import javafx.scene.Parent;
+import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
+import netscape.javascript.JSObject;
 
-public class CodeArea extends Parent {
+public class CodeArea {
+    private JSObject editor;
+    private JSObject session;
+    private WebView webView;
+    private WebEngine webEngine;
+    
     public CodeArea() {
         super();
-
+    }
+    
+    public void init() {
         final String html = CodeArea.class.getResource("editor.html").toExternalForm();
         final java.net.URI uri = java.nio.file.Paths.get(html).toAbsolutePath().toUri();
-        final WebView root = new WebView();
-        root.getEngine().load(html);
-        System.out.println(html);
-        this.getChildren().add(root);
+        
+        webView = new WebView();
+        webEngine = webView.getEngine();
+        webEngine.load(html);
+        
+    }
+    
+    public WebView getWebView() {
+        return this.webView;
+    }
+    
+    public JSObject fetchEditor() {
+        if(editor == null) {
+            Object temp = webEngine.executeScript("editor;");
+            if(temp instanceof JSObject) {
+                editor = (JSObject) temp;
+            }
+        }
+        return editor;
+    }
+    
+    public JSObject fetchSession() {
+        if(session == null) {
+            Object temp = webEngine.executeScript("editor.session;");
+            if(temp instanceof JSObject) {
+                session = (JSObject) temp;
+            }
+        }
+        return session;
+    }
+    
+    public void setValue(String value) {
+        if(fetchEditor() != null) {
+            editor.call("setValue", value);
+        }
+    }
+    
+    public String getValue() {
+        if(fetchEditor() != null) {
+            return (String) editor.call("getValue"); //TODO add check
+        } else {
+            return null;
+        }
+    }
+    
+    public void setMode(String mode) {
+        if(fetchSession() != null) {
+            session.call("setMode", mode);
+        }
+    }
+    
+    public String getMode() {
+        if(fetchSession() != null) {
+            return (String) session.eval("this.getMode().$id;");
+        } else {
+            return null;
+        }
     }
 }
--- src/test/java/org/almibe/codearea/testfx/UITests.java
+++ src/test/java/org/almibe/codearea/testfx/UITests.java
@@ -6,14 +6,49 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.loadui.testfx.GuiTest;
 
-public class UITests extends GuiTest {
-    @Override
-    public Parent getRootNode() {
-        return new CodeArea();
-    }
+//Giving up on tests for right now since I've been getting weird timing issues
+//in tests that I don't get when running it normally.
+public class UITests {// extends GuiTest {
+    CodeArea codeArea = new CodeArea();
     
-    @Test
-    public void dummyTest() {
-        Assert.assertEquals(true,true);
-    }
+    
+//    @Override
+//    public Parent getRootNode() {
+//        codeArea.init();
+//        codeArea.setId("codeArea");
+//        return codeArea.getWebView();
+//    }
+//    
+//    @Test
+//    public void editor_should_start_empty() {
+//        Assert.assertEquals("", codeArea.getValue());        
+//    }
+//    
+//    @Test
+//    public void set_and_get_text_test() {
+//        codeArea.setValue("test");
+//        Assert.assertEquals("test", codeArea.getValue());
+//        codeArea.setValue("");
+//        Assert.assertEquals("", codeArea.getValue());
+//    }
+//    
+//    @Test
+//    public void keyboard_test() {
+//        click("#codeArea").type("test");
+//        Assert.assertEquals("test", codeArea.getValue());
+//    }
+    
+//    @Test
+//    public void set_and_get_mode() {
+//        codeArea.setMode("ace/mode/javascript");
+//        sleep(1000);
+//        Assert.assertEquals("ace/mode/javascript", codeArea.getMode());
+//    }
+    
+    
+//    @Test
+//    public void set_and_get_mode() {
+//        codeArea.setMode("ace/theme/twilight");
+//        Assert.assertEquals("ace/theme/twilight", codeArea.getMode());
+//    }
 }
