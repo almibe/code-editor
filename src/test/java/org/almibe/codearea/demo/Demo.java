4b38e761a146a4127d9f08aff26b9e486107949d
Alex Berry <berry3@indiana.edu>
2014-12-04 02:51:21 -0500

    renaming implementation class

--- src/test/java/org/almibe/codearea/demo/Demo.java
+++ src/test/java/org/almibe/codearea/demo/Demo.java
@@ -3,14 +3,14 @@ package org.almibe.codearea.demo;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import org.almibe.codearea.AceCodeArea;
+import org.almibe.codearea.CodeMirrorArea;
 import org.almibe.codearea.CodeArea;
 
 public class Demo extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        CodeArea codeArea = new AceCodeArea();
+        CodeArea codeArea = new CodeMirrorArea();
         Scene scene = new Scene(codeArea.getWidget());
         primaryStage.setScene(scene);
         codeArea.init();
