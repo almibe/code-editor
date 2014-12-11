bf6439c42e1446550ab6bf62d7536bbc73a32959
Alex Berry <berry3@indiana.edu>
2014-12-10 23:20:15 -0500

    moving functions to codeeditor module

--- src/main/resources/org/almibe/codeeditor/html/codeeditor.js
+++ src/main/resources/org/almibe/codeeditor/html/codeeditor.js
@@ -5,4 +5,62 @@ define(["codemirror-4.8/lib/codemirror", "codemirror-4.8/addon/display/fullscree
     codeMirror.setOption("fullScreen", true);
     codeMirror.setOption("styleActiveLine", true);
     CodeMirror.modeURL = "codemirror-4.8/mode/%N/%N.js";
+
+
+    return {
+        setTheme: function(theme) {
+            if (theme == 'default') {
+              codeMirror.setOption("theme", 'default');
+              return;
+            }
+            var sheets = document.styleSheets;
+            var exists = false;
+            for (var sheet in sheets) {
+              if (sheet && sheet.href && sheet.href.endsWith("/" + theme + ".css")) {
+                exists = true;
+                break;
+              }
+            }
+            if (!exists) {
+              var link = document.createElement("link");
+              link.rel = "stylesheet";
+              link.type = "text/css";
+              link.href = "codemirror-4.8/theme/" + theme + ".css";
+              document.getElementsByTagName("head")[0].appendChild(link);
+            }
+            codeMirror.setOption("theme", theme);
+        },
+
+        importJS: function(file, callback) {
+
+        },
+
+        setMode: function(mode) {
+            var mimeModes = Object.keys(CodeMirror.mimeModes);
+            var modeNames = Object.keys(CodeMirror.modes);
+            var allModeNames = mimeModes.concat(modeNames);
+            if (allModeNames.indexOf(modeName)) {
+              codeMirror.setOption("mode", modeName);
+            }
+            change(modeName);
+        },
+
+        //modified from demo/loadmode.html
+        change: function(val) {
+            var m, mode, spec;
+            if (/\//.test(val)) {
+              var info = CodeMirror.findModeByMIME(val);
+              if (info) {
+                mode = info.mode;
+                spec = val;
+              }
+            } else {
+              mode = spec = val;
+            }
+            if (mode) {
+              codeMirror.setOption("mode", spec);
+              CodeMirror.autoLoadMode(codeMirror, mode);
+            }
+        }
+    }
 });
