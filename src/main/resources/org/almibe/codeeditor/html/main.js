1aa55a9e3eebf67e33001b9600cc7390e2a599d2
Alex Berry <berry3@indiana.edu>
2014-12-10 23:09:42 -0500

    moving init code to code editor module

--- src/main/resources/org/almibe/codeeditor/html/main.js
+++ src/main/resources/org/almibe/codeeditor/html/main.js
@@ -1,10 +1,4 @@
-require(["codemirror-4.8/lib/codemirror", "codemirror-4.8/addon/display/fullscreen", "codemirror-4.8/addon/selection/active-line",
-    "codemirror-4.8/addon/mode/loadmode", "codemirror-4.8/mode/meta"],
-    function(CodeMirror) {
-        var codeMirror = CodeMirror(document.body);
-        codeMirror.setOption("lineNumbers", true);
-        codeMirror.setOption("fullScreen", true);
-        codeMirror.setOption("styleActiveLine", true);
-        CodeMirror.modeURL = "codemirror-4.8/mode/%N/%N.js";
+require(["codeeditor"],
+    function() {
     }
 );
