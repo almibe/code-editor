fc1d2a72ab4f50f6f79983c487c41d6c1d23639d
Alex Berry <berry3@indiana.edu>
2014-12-14 20:36:28 -0500

    moving code mirror to test resources

---
+++ src/test/resources/html/codeeditor.js
@@ -0,0 +1,69 @@
+define(["codemirror/lib/codemirror", "codemirror/addon/display/fullscreen", "codemirror/addon/selection/active-line",
+            "codemirror/addon/mode/loadmode", "codemirror/mode/meta"], function(CodeMirror) {
+    var codeMirror = CodeMirror(document.body);
+    codeMirror.setOption("lineNumbers", true);
+    codeMirror.setOption("fullScreen", true);
+    codeMirror.setOption("styleActiveLine", true);
+    CodeMirror.modeURL = "codemirror/mode/%N/%N.js";
+
+    //modified from demo/loadmode.html
+    function changeMode(val) {
+        var m, mode, spec;
+        if (/\//.test(val)) {
+          var info = CodeMirror.findModeByMIME(val);
+          if (info) {
+            mode = info.mode;
+            spec = val;
+          }
+        } else {
+          mode = spec = val;
+        }
+        if (mode) {
+          codeMirror.setOption("mode", spec);
+          CodeMirror.autoLoadMode(codeMirror, mode);
+        }
+    }
+
+    return {
+        "codeMirror": codeMirror,
+        setTheme: function(theme) {
+            if (theme == 'default') {
+              codeMirror.setOption("theme", 'default');
+              return;
+            }
+            var sheets = document.styleSheets;
+            var exists = false;
+            for (var sheet in sheets) {
+              if (sheet && sheet.href && sheet.href.endsWith("/" + theme + ".css")) {
+                exists = true;
+                break;
+              }
+            }
+            if (!exists) {
+              var link = document.createElement("link");
+              link.rel = "stylesheet";
+              link.type = "text/css";
+              link.href = "codemirror/theme/" + theme + ".css";
+              document.getElementsByTagName("head")[0].appendChild(link);
+            }
+            codeMirror.setOption("theme", theme);
+        },
+
+        importJSModules: function(modules, callback) {
+            require(modules, function() {
+                callback.run();
+            });
+        },
+
+        setMode: function(modeName) {
+            var mimeModes = Object.keys(CodeMirror.mimeModes);
+            var modeNames = Object.keys(CodeMirror.modes);
+            var allModeNames = mimeModes.concat(modeNames);
+            if (allModeNames.indexOf(modeName) >= 0) {
+              codeMirror.setOption("mode", modeName);
+            } else {
+              changeMode(modeName);
+            }
+        }
+    }
+});
---
+++ src/test/resources/html/main.js
@@ -0,0 +1,4 @@
+require(["codeeditor"],
+    function() {
+    }
+);
